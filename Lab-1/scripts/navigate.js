28e133d331285f1334a685268220837ca9071f9d
RomanPR <prikhodjko.roman@gmail.com>
2025-09-04 03:12:50 +0300

    Initial commit
    Added navbar, page files, basic styles, navigation functionality

---
+++ Lab-1/scripts/navigate.js
@@ -0,0 +1,9 @@
+const gotoMainPageButton = document.getElementById('goto-main-page').addEventListener('click', () => {
+    window.location.href = './index.html';
+});
+const gotoAboutPageButton = document.getElementById('goto-about-page').addEventListener('click', () => {
+    window.location.href = './about.html';
+});
+const gotoContactsPageButton = document.getElementById('goto-contacts-page').addEventListener('click', () => {
+    window.location.href = './contacts.html';
+});
