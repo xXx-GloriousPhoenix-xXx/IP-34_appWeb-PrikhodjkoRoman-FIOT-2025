d2f2bc0c99da85522ae04912575359d0f8b335d5
RomanPR <prikhodjko.roman@gmail.com>
2025-10-15 08:57:38 +0300

    fix2

--- campout/src/components/Report/Header/Header.jsx
+++ campout/src/components/Report/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
                 </div>
             </div>
             <div className={cl.PfpWrapper}>
-                <ImageReference imgSrc={`${process.env.PUBLIC_URL}img/report/pfp.jpg`}/>
+                <ImageReference imgSrc={`${process.env.PUBLIC_URL}/img/report/pfp.jpg`}/>
             </div>
         </header>
     );
